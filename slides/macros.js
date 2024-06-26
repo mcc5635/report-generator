5a3e5a103e46de951774368438e6d68e1fc1cd8b
Michael Chase <themikechase@gmail.com>
2024-06-26 09:03:30 -0700

    Delete slides directory

--- slides/macros.js
+++
@@ -1,8 +0,0 @@
-remark.macros.scale = function(w, alt="") {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + w + ';" alt="' + alt + '" />';
-};
-remark.macros.scaleh = function(h, alt="") {
-  var url = this;
-  return '<img src="' + url + '" style="height: ' + h + ';" alt="' + alt + '" />';
-};
